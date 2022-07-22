5e9ce2541373f6c52a0ab85b2fae06b636f46028
unknown <iamhasan9501@gmail.com>
2022-07-23 03:23:31 +0600

    nav done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route, Link } from "react-router-dom";
+import CardDetails from './components/CardDetails';
+import Cards from './components/Cards';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Cards></Cards>}></Route>
+        <Route path='/cart' element={<CardDetails></CardDetails>}></Route>
+
+      </Routes>
     </div>
   );
 }
